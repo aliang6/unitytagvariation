985ca2c8115c5dcb09844393b408668bdc71b9fe
ivlin <ivlin112@gmail.com>
2017-01-04 12:54:23 -0500

    cooldowns

--- Tag/Assets/MainScene/Scripts/Player2Movement.cs
+++ Tag/Assets/MainScene/Scripts/Player2Movement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player2Movement : MonoBehaviour {
 
+	private float QCOOLDOWN = 10.0f;
+	private float ECOOLDOWN = 10.0f;
+
 	public float speed;
 	private int tagCount;
 	private Rigidbody2D rb2d;
+	private float qCooldownTimer, eCooldownTimer;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
 		tagCount = 0;
-
 	}
 
 	// Update is called once per frame
@@ -21,9 +24,19 @@ public class Player2Movement : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical2");
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		rb2d.AddForce (movement);
-
-		if (Input.GetKey (KeyCode.Q)) {
-			rb2d.AddForce (movement * 2, ForceMode2D.Impulse);
+		if (Input.GetKey (KeyCode.Q) && qCooldownTimer <= 0) {
+			rb2d.AddForce (movement * 3, ForceMode2D.Impulse);
+			qCooldownTimer= QCOOLDOWN;
+		}
+		if (Input.GetKey (KeyCode.E) && eCooldownTimer <= 0) {
+			Vector3 velocity = rb2d.velocity;
+			rb2d.velocity *= -1;
+			eCooldownTimer= ECOOLDOWN;
 		}
 	}	
+
+	void Update () {
+		qCooldownTimer-= Time.deltaTime;
+		eCooldownTimer-= Time.deltaTime;
+	}
 }
--- Tag/Assets/MainScene/Scripts/PlayerMovement.cs
+++ Tag/Assets/MainScene/Scripts/PlayerMovement.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+	private float ENTERCOOLDOWN = 10.0f;
+	private float SHIFTCOOLDOWN = 10.0f;
+
 	public float speed;
 
 	private int tagCount;
 	private Rigidbody2D rb2d;
+
+	private float enterCooldownTimer, shiftCooldownTimer;
+
 
 	// Use this for initialization
 	void Start () {
@@ -23,11 +29,23 @@ public class PlayerMovement : MonoBehaviour {
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		rb2d.AddForce (movement);
 
-		if (Input.GetKey (KeyCode.Return) ||
-			Input.GetKey (KeyCode.KeypadEnter)) {
-			rb2d.AddForce (movement * 2, ForceMode2D.Impulse);
+		if ((Input.GetKey (KeyCode.Return) ||
+			Input.GetKey (KeyCode.KeypadEnter)) 
+			&& enterCooldownTimer <=0) {
+			rb2d.AddForce (movement * 3, ForceMode2D.Impulse);
+			enterCooldownTimer= ENTERCOOLDOWN;
 		}
-	
+		if (Input.GetKey (KeyCode.RightShift) && shiftCooldownTimer <= 0) {
+			Vector3 velocity = rb2d.velocity;
+			rb2d.velocity *= -1;
+			shiftCooldownTimer= SHIFTCOOLDOWN;
+		}
+
+	}
+
+	void Update () {
+		enterCooldownTimer-= Time.deltaTime;
+		shiftCooldownTimer-= Time.deltaTime;
 	}
 
 }
