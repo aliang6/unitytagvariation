d7266ac63147b5b3b880f07252c10cb459fe38c0
ivlin <ivlin112@gmail.com>
2017-01-04 15:23:18 -0500

    hope i didnt breeak nything

---
+++ Tag/Assets/MainScene/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D coll){
+		//here increment collided object's tagcounter
+	}
+}
