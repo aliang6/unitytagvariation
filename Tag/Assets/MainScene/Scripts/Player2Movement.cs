8cce9ff4f892a2bb682b44016ce67e9e4884a751
Andy Liang <andy.liang.1@stonybrook.edu>
2017-01-04 16:40:55 -0500

    Added Tag Counter

--- Tag/Assets/MainScene/Scripts/Player2Movement.cs
+++ Tag/Assets/MainScene/Scripts/Player2Movement.cs
@@ -7,27 +7,28 @@ public class Player2Movement : MonoBehaviour {
 
 	private float QCOOLDOWN = 2.0f;
 	private float ECOOLDOWN = 2.0f;
-	private float RCOOLDOWN = 2.0f;
+	private float RCOOLDOWN = 3.0f;
+	private float IMMUNEDURATION = 1.0f;
 
 	public Rigidbody2D projectile;
 	public float speed;
 	public Rigidbody2D rb2d2;
 	public UnityEngine.UI.Text speedText, qCooldown, eCooldown, tagNumber;
 
-	private bool collide;
+	private bool immune;
 	private int tagCount;
 	private Rigidbody2D rb2d;
-	private float qCooldownTimer, eCooldownTimer, rCooldownTimer;
+	private float immuneCooldownTimer, qCooldownTimer, eCooldownTimer, rCooldownTimer;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
 		tagCount = 0;
+		immune = false;
 		tagNumber.text = tagCount.ToString ();
 		speedText.text = (Mathf.CeilToInt(rb2d.velocity.magnitude)).ToString();
 		qCooldown.text = qCooldownTimer.ToString ();
 		eCooldown.text = eCooldownTimer.ToString ();
-		collide = false;
 	}
 
 	// Update is called once per frame
@@ -35,10 +36,12 @@ public class Player2Movement : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal2");
 		float moveVertical = Input.GetAxis ("Vertical2");
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		rb2d.AddForce (movement * 3);
+		rb2d.AddForce (movement * speed);
 
 		if (Input.GetKey (KeyCode.Q) && qCooldownTimer <= 0) {
+			immune = true;
 			rb2d.AddForce (movement * 5, ForceMode2D.Impulse);
+			immuneCooldownTimer = IMMUNEDURATION;
 			qCooldownTimer= QCOOLDOWN;
 		}
 
@@ -73,7 +76,21 @@ public class Player2Movement : MonoBehaviour {
 		else {
 			rCooldownTimer -= Time.deltaTime;
 		}
+		if (immuneCooldownTimer <= 0) {
+			immune = false;
+			immuneCooldownTimer = 0;
+		} 
+		else {
+			immuneCooldownTimer -= Time.deltaTime;
+		}
 		qCooldown.text = (Mathf.CeilToInt(qCooldownTimer)).ToString ();
 		eCooldown.text = (Mathf.CeilToInt(eCooldownTimer)).ToString ();
 	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			tagCount++;
+		}
+		tagNumber.text = tagCount.ToString ();
+	} 
 }
--- Tag/Assets/MainScene/Scripts/PlayerMovement.cs
+++ Tag/Assets/MainScene/Scripts/PlayerMovement.cs
@@ -7,7 +7,8 @@ public class PlayerMovement : MonoBehaviour {
 
 	private float ENTERCOOLDOWN = 2.0f;
 	private float SHIFTCOOLDOWN = 2.0f;
-	private float SLASHCOOLDOWN = 10.0f;
+	private float SLASHCOOLDOWN = 3.0f;
+	private float IMMUNEDURATION = 1.0f;
 
 	public float speed;
 	public Rigidbody2D rb2d2;
@@ -15,22 +16,22 @@ public class PlayerMovement : MonoBehaviour {
 
 	public Rigidbody2D projectile;
 
-	private bool collide;
+	private bool immune;
 	private int tagCount;
 	private Rigidbody2D rb2d;
 
-	private float enterCooldownTimer, shiftCooldownTimer, slashCooldownTimer;
+	private float immuneCooldownTimer, enterCooldownTimer, shiftCooldownTimer, slashCooldownTimer;
 
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
 		tagCount = 0;
-
+		immune = false;
+		tagNumber.text = tagCount.ToString ();
 		speedText.text = (Mathf.CeilToInt(rb2d.velocity.magnitude)).ToString();
 		enterCooldown.text = enterCooldownTimer.ToString ();
 		shiftCooldown.text = shiftCooldownTimer.ToString ();
-		collide = false;
 	}
 	
 	// Update is called once per frame
@@ -38,12 +39,14 @@ public class PlayerMovement : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		rb2d.AddForce (movement * 3);
+		rb2d.AddForce (movement * speed);
 
 		if ((Input.GetKey (KeyCode.Return) ||
 			Input.GetKey (KeyCode.KeypadEnter)) 
 			&& enterCooldownTimer <=0) {
+			immune = true;
 			rb2d.AddForce (movement * 5, ForceMode2D.Impulse);
+			immuneCooldownTimer = IMMUNEDURATION;
 			enterCooldownTimer= ENTERCOOLDOWN;
 		}
 		if (Input.GetKey (KeyCode.RightShift) && shiftCooldownTimer <= 0) {
@@ -60,7 +63,6 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	void Update () {
-		slashCooldownTimer-= Time.deltaTime;
 		if (enterCooldownTimer <= 0) {
 			enterCooldownTimer = 0;
 		}
@@ -73,12 +75,27 @@ public class PlayerMovement : MonoBehaviour {
 		else {
 			shiftCooldownTimer -= Time.deltaTime;
 		}
+		if (slashCooldownTimer <= 0) {
+			slashCooldownTimer = 0;
+		} 
+		else {
+			slashCooldownTimer -= Time.deltaTime;
+		}
+		if (immuneCooldownTimer <= 0) {
+			immune = false;
+			immuneCooldownTimer = 0;
+		} 
+		else {
+			immuneCooldownTimer -= Time.deltaTime;
+		}
 		enterCooldown.text = (Mathf.CeilToInt(enterCooldownTimer)).ToString ();
 		shiftCooldown.text = (Mathf.CeilToInt(shiftCooldownTimer)).ToString ();
 	}
-
-	/*void OnCollisionEnter (Rigidbody2D rdbd2) {
 		
-	} */
-
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Player" && immune == false) {
+			tagCount++;
+		}
+		tagNumber.text = tagCount.ToString ();
+	} 
 }
